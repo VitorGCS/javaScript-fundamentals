e8600a39ac419040d45d1e827ee4a54b08f74501
Vitor Silva <vitorgilcs@gmail.com>
2020-08-09 15:42:24 +0100

    objects and methods

--- objects.js
+++ objects.js
@@ -2,6 +2,7 @@
  * Objects and properties
  */
 
+ /*
  //object literal
  var john = {
      firstName:'John',
@@ -10,7 +11,7 @@
      family:['Jane','Mark','Bob','Emily'],
      job: 'teacher',
      isMarried: false
- }
+ };
 
  console.log(john.firstName+' '+john.lastName);
  console.log(john['firstName'])
@@ -27,3 +28,25 @@
  jane.birthYear = 1969;
  jane['lastName'] = 'Smith';
  console.log(jane);
+ */
+
+ /******************************************
+  * Objects and methods
+  */
+
+ var john = {
+    firstName:'John',
+    lastName:'Smith',
+    birthYear:1988,
+    family:['Jane','Mark','Bob','Emily'],
+    job: 'teacher',
+    isMarried: false,
+    calcAge: function(){
+       this.age = 2020 - this.birthYear;
+    }
+};
+
+//john.age =john.calcAge();
+john.calcAge();
+console.log(john);
+
